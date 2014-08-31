5f1779355d31f9ab00094a31820c0d64ee2e0593
Mike Maelzer <mmaelzer@gmail.com>
2014-08-31 14:23:46 -0700

    initial commit

---
+++ readme.md
@@ -0,0 +1,160 @@
+surely
+======
+
+Can I add runtime type checking to JavaScript?  
+  
+Surely you can't be serious.  
+  
+I am serious... and don't call me surely.
+
+
+### Install
+```
+npm install surely
+```  
+
+----------------------------
+
+
+Example
+--------
+
+```javascript
+var fs = require('fs');
+var Surely = require('surely');
+
+
+// --- Asynchronous error generation ---
+var read = Surely
+            .string('filename')   // First argument must be a string
+            .object('options?')   // Second argument can optionally be an object
+            .callback('callback') // Second/Third argument must be a callback function
+            .wrap(fs.readFile);
+
+read(null, function(err, data) {
+  console.log(err);
+  // [TypeError: Expected string for "filename"]
+});
+
+
+// --- Synchronous error generation ---
+var readSync = Surely.string('filename').object('options?').wrap(fs.readFileSync);
+var data = readSync(['foo.txt', 'bar.tar.gz']);
+console.log(data);
+// [TypeError: Expected string for "filename"]
+
+
+// --- Using defaults ---
+var random = Surely.number('min', 0).number('max', 100).wrap(function(min, max) {
+  return min + Math.floor(Math.random() * (max - min + 1));
+});
+
+console.log(random());
+// A random value between 0 and 100
+
+console.log(random(10));
+// A random value between 10 and 100
+
+console.log(random(10, 20));
+// A random value between 10 and 20
+```
+
+
+Types
+------
+Surely comes with a set of default types. You can define your own types or override the default types with your own methods.
+
+#### Default Types
+* array
+* bool
+* callback
+* date
+* func
+* number
+* object
+* regex
+* string
+
+#### Surely.add(name, testFunction)
+You can add types by using the `add` method with a `String` name and `Function` test function.
+```javascript
+var Surely = require('surely');
+
+Surely.add('int', function(val) {
+  return typeof val === 'number' && Math.floor(val) === val;
+});
+
+var double = Surely.int('num').wrap(function(num) {
+  return num * 2;
+});
+
+console.log(double(3.1));
+// [TypeError: Expected int for 'num']
+```
+
+#### Surely.add(object)
+You can also add types by using the `add` method with an object that contains key names and function values. Internally, `surely` uses this method to add the default types.
+```javascript
+var Surely = require('surely');
+
+var typetest = function(type) {
+  return function(val) {
+    return typeof val === type;
+  };
+};
+
+Surely.add({
+  'number': typetest('number'),
+  'string': typetest('string'),
+  'array': Array.isArray
+});
+```
+
+Usage
+-------
+Surely uses a [builder pattern](http://addyosmani.com/resources/essentialjsdesignpatterns/book/#builderpatternjquery) to construct a wrapped function.
+
+#### Type(name, [defaultValue])
+
+Each `type` (from the last of **Default Types** above) is a method that can be called with one required `name` argument and one optional `defaultValue` argument. To make an argument optional, append a `?` to the end of the `name` argument.
+
+```javascript
+var Surely = require('surely');
+
+Surely
+  .string('message')                // 'message' string param
+  .object('data?')                  // 'data' object param that is optional
+  .function('logger', console.log)  // a logging function that defaults to console.log
+  .wrap(
+function(message, opt_data, logger) {
+  if (opt_data) {
+    logger(message, opt_data);
+  } else {
+    logger(message);
+  }
+});
+```
+
+
+The MIT License
+===============
+
+Copyright (c) 2014 Michael Maelzer
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ surely.js
@@ -0,0 +1,260 @@
+var _ = require('lodash');
+
+/**
+ *  @param {String=} opt_argName
+ *  @param {String=} opt_type
+ *  @constructor
+ */
+function Surely(opt_argName, opt_type, opt_default) {
+  this.args = opt_argName && opt_type ? [this._buildArg(opt_argName, opt_type, opt_default)] : [];
+}
+
+/**
+ *  Provides a mechnism for 'types' to be added to the global Surely object
+ *  @param {String|Object}
+ *  @param {Function=} testFunction
+ *  @public
+ */
+Surely.add = function(type, testFunction) {
+  var obj = type;
+  if (_.isString(type)) {
+    obj = {};
+    obj[type] = testFunction;
+  }
+
+  // Build up the Surely object with methods for types
+  Object.keys(obj).forEach(function(type) {
+    var factory = builderFactory(type);
+    Surely[type] = factory;
+    Surely.prototype[type] = factory;
+    Surely.types[type] = obj[type];
+  });
+
+  return this;
+};
+
+/**
+ *  An enum of type checking methods to call on a list of arguments.
+ *  @enum {Function}
+ */
+Surely.types = {};
+
+// Default type list
+Surely.add({
+  'array': Array.isArray,
+  'bool': _.isBoolean,
+  'callback': _.isFunction,
+  'date': _.isDate,
+  'func': _.isFunction,
+  'number': _.isNumber,
+  'object': _.isObject,
+  'regex': _.isRegExp,
+  'string': _.isString
+});
+
+/**
+ *  @param {String} name
+ *  @param {String} type
+ *  @param {*=} opt_default
+ *  @return {Object}
+ *  @private
+ */
+Surely.prototype._buildArg = function(name, type, opt_default) {
+  return {
+    type: type,
+    name: name.replace(/\?/g, ''),
+    optional: /\?$/.test(name) || arguments.length === 3,
+    default: opt_default
+  };
+};
+
+/**
+ *  @param {Function} fn
+ *  @return {Function}
+ *  @public
+ */
+Surely.prototype.wrap = function(fn) {
+  var self = this;
+  var boundFunction = function surelyWrap() {
+    // optimization to avoid passing arguments around since it causes a performance hit
+    // to V8 per https://github.com/petkaantonov/bluebird/wiki/Optimization-killers
+    var args = [];
+    for (var i = 0; i < arguments.length; i++) {
+      args[i] = arguments[i];
+    }
+    var parsedArgs = self.parse(args);
+    if (parsedArgs instanceof Error) {
+      return self._handleError(parsedArgs, args);
+    } else {
+      return fn.apply(this, parsedArgs);
+    }
+  };
+  boundFunction.expects = this.args;
+  return boundFunction;
+};
+
+/**
+ *  @param {Error} err
+ *  @param {Arguments} args
+ *  @return {Error=}
+ */
+Surely.prototype._handleError = function(err, args) {
+  var callback = this._getCallbackFromArguments(args);
+  if (callback) {
+    callback(err);
+  } else {
+    return err;
+  }
+};
+
+/** @return {Boolean} **/
+Surely.prototype._hasCallback = function() {
+  return _.chain(this.args).pluck('type').contains('callback').value();
+};
+
+/**
+ *  @param {Array.<Object>} args
+ *  @return {Function}
+ */
+Surely.prototype._getCallbackFromArguments = function(args) {
+  var indexOfCallback = _.chain(this.args)
+                          .pluck('type')
+                          .indexOf('callback')
+                          .value();
+  var callback = args[indexOfCallback];
+  return _.isFunction(callback) ? callback : null;
+};
+
+/**
+ *  Parse arguments or an object and return an arguments array that can be then
+ *  used on a function via func.apply(context, arg). Return an Error
+ *  if arguments do not meet the validation rules in this.args
+ *  @param {Object} options
+ *  @return {Array.<*>|Error}
+ *  @public
+ */
+Surely.prototype.parse = function(args) {
+  if ((_.isObject(args) && !_.isArray(args)) || this._firstArgIsObjectWithParams(args)) {
+    return this._parseObject(_.isObject(args) && args.length ? args[0] : args);
+  } else {
+    return this._parseArguments(args);
+  }
+};
+
+
+/**
+ *  @param {Array.<*>} args
+ *  @return {Boolean}
+ */
+Surely.prototype._firstArgIsObjectWithParams = function(args) {
+  return args.length === 1 &&
+         _.isObject(args[0]) &&
+         this.args[0].type !== 'object';
+};
+
+/** @return {Number} **/
+Surely.prototype._getRequiredArgsLength = function() {
+  return _.reject(this.args, function(arg) {
+    return arg.optional;
+  }).length;
+};
+
+/**
+ *  @param {*} val
+ *  @return {Boolean}
+ */
+Surely.prototype._notValue = function(val) {
+  return _.isUndefined(val) || _.isNull(val);
+};
+
+/**
+ *  @param {Object} argsToParse
+ *  @return {Array.<*>|Error}
+ *  @private
+ */
+Surely.prototype._parseArguments = function(argsToParse) {
+  // If the arguments and our list of args to check are different
+  // in length, the test has already failed
+  var required = this._getRequiredArgsLength();
+  if (required > argsToParse.length) {
+    return new Error('Incorrect number of parameters. Expected ' +
+                     required + ' found ' + argsToParse.length + '.');
+  }
+
+  var parsedArgs = [];
+  for (var i = 0; i < this.args.length; i++) {
+    var arg = this.args[i];
+    var val = argsToParse[i];
+
+    if (this._testValueWithType(val, arg.type)) {
+      parsedArgs.push(val);
+    } else if (arg.optional && this._notValue(val)) {
+      parsedArgs.push(arg.default);
+    } else {
+      return new TypeError('Expected ' + arg.type + ' for "' + arg.name + '"');
+    }
+  }
+  return parsedArgs;
+};
+
+/**
+ *  @param {Object} options
+ *  @return {Array.<*>|Error}
+ *  @private
+ */
+Surely.prototype._parseObject = function(options) {
+  // If the number of keys in the options object is less than
+  // the number of args to check, the test has already failed
+  var keys = _.isObject(options) ? Object.keys(options) : [];
+  var required = this._getRequiredArgsLength();
+  if (keys.length < required) {
+    return new Error('Incorrect number of parameters. Expected ' +
+                     required + ' found ' + keys.length + '.');
+  }
+
+  var parsedArgs = [];
+  for (var i = 0; i < this.args.length; i++) {
+    var arg = this.args[i];
+    if (arg.name in options) {
+      var val = options[arg.name];
+      if (this._testValueWithType(val, arg.type)) {
+        parsedArgs.push(val);
+      } else {
+        return new TypeError('Parameter "' + arg.name +
+          '" is not the correct type. Expected type "' + arg.type + '"');
+      }
+    } else if (arg.optional) {
+      parsedArgs.push(arg.default);
+    } else {
+      return new Error('Missing parameter: ' + arg.name);
+    }
+  }
+  return parsedArgs;
+};
+
+/**
+ *  @param {*} value
+ *  @param {String} type
+ *  @return {Boolean}
+ *  @private
+ */
+Surely.prototype._testValueWithType = function(value, type) {
+  return Surely.types[type](value);
+};
+
+/**
+ *  @param {String} type
+ *  @return {Function(String, Boolean=)}
+ */
+function builderFactory(type) {
+  return function(name, opt_default) {
+    if (this instanceof Surely) {
+      this.args.push(this._buildArg(name, type, opt_default));
+      return this;
+    } else {
+      return new Surely(name, type, opt_default);
+    }
+  };
+}
+
+module.exports = Surely;
