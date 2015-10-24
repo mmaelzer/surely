143d7fe34b885aedd9b8de8352fb52057837e038
Mike Maelzer <mmaelzer@gmail.com>
2015-10-24 14:14:29 -0700

    - Change tests to use nodeunit - Add coverage script for coveralls - Update travis config

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
-var test = require('tape');
-var Surely = require('./surely');
+var Surely = (function(coverage) {
+  return coverage
+    ? require('./surely-cov')
+    : require('./surely');
+})(process.env.USE_COVERAGE);
 
-test('surely', function(t) {
-  t.plan(14);
-
+module.exports.testNumbers = function(t) {
   Surely.add('int', function(val) {
     return typeof val === 'number' && Math.floor(val) === val;
   });
@@ -31,6 +32,10 @@ test('surely', function(t) {
   t.ok(half(3) instanceof Error, 'Type added with Surely.add(object) returns an Error on invalid type');
   t.equal(half(4), 2, 'Type added with Surely.add(object) calls the underlying function properly');
 
+  t.done();
+};
+
+module.exports.testAsync = function(t) {
   var asyncFn = Surely.array('arr').callback('callback').wrap(function(arr, callback) {
     callback(null, arr.slice());
   });
@@ -43,6 +48,10 @@ test('surely', function(t) {
     t.deepEqual(array, [1,2,3], 'Surely with callback param passes values to the given callback');
   });
 
+  t.done();
+};
+
+module.exports.testObjects = function(t) {
   var fnWithObject = Surely.number('num').string('str').regex('reg?').wrap(function(num, str, reg) {
     return {
       num: num,
@@ -55,11 +64,14 @@ test('surely', function(t) {
     num: 123,
     str: 'foo',
     reg: /^this\smy\sregex$/
-  }
+  };
 
   t.deepEqual(fnWithObject(testObject), testObject, 'Surely called with an object that matches params is valid');
   t.ok(fnWithObject({num: 213}) instanceof Error, 'Surely called with an object that does not match params is not valid');
+  t.done();
+};
 
+module.exports.testOptional = function(t) {
   var optional = Surely.date('time?').wrap(function(time) {
     return time ? +time : 0;
   });
@@ -67,8 +79,12 @@ test('surely', function(t) {
   t.equal(optional(), 0, 'Surely with an optional param is valid when that param is not provided');
 
   var now = new Date();
-  t.equal(optional(now), +now, 'Surely with an optinal param is valid when that param is provied');
+  t.equal(optional(now), +now, 'Surely with an optional param is valid when that param is provied');
 
+  t.done();
+};
+
+module.exports.testDefaultParams = function(t) {
   var sqr = Surely.number('sumNumber', 7).wrap(function(sumNumber) {
     return sumNumber * sumNumber;
   });
@@ -77,4 +93,5 @@ test('surely', function(t) {
 
   t.equal(sqr(), 49, 'Surely with a default param is valid when that param is not provieded');
 
-});
+  t.done();
+};
